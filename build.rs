3ba3f2471af863ff9eb7145b8b594f6da80233dd
Gijs Van Laer <gijs.vanlaer@gmail.com>
2019-12-17 10:29:20 -0500

    mpcwrapper: bindgen in build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+extern crate bindgen;
+
 use std::env;
 
 fn main() {
@@ -5,4 +7,19 @@ fn main() {
 
     println!("cargo:rustc-link-search={}/deps/root/lib", project_dir); // the "-L" flag
     println!("cargo:rustc-link-lib=token-utils"); // the "-l" flag
+
+    // Create bindings
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("deps/emp-sh2pc/tokens/tokens.h")
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    bindings
+        .write_to_file("src/bindings.rs")
+        .expect("Couldn't write bindings!");
 }
