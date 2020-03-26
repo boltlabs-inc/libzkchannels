27d1ed00d53bb28c1f7b3276cb5e43d1b5e5a6e1
J. Ayo Akinyele <ayo@boltlabs.io>
2020-03-26 16:47:51 -0400

    minor updates

--- cli/customer_pay.sh
+++ cli/customer_pay.sh
@@ -3,4 +3,4 @@
 mode=release
 #amount=${1:-100}
 
-target/$mode/zkchannels-mpc pay --party CUST --other-port 12347 --own-port 12346 --amount=$1 -v
+../target/$mode/zkchannels-mpc pay --party CUST --other-port 12347 --own-port 12346 --amount=$1 -v
