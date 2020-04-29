a81d902730629195e6294b159cc7834e9498efda
J. Ayo Akinyele <ayo@boltlabs.io>
2020-04-29 11:00:28 -0400

    update test_gowrapper to get package github.com/stretchr/testify/assert

--- test_gowrapper.sh
+++ test_gowrapper.sh
@@ -2,4 +2,5 @@
 
 export LD_LIBRARY_PATH="-L$(pwd)/target/release"
 export CGO_LDFLAGS="-L$(pwd)/target/release"
+go get -v github.com/stretchr/testify/assert
 go test -v libzkchannels.go libzkchannels_test.go
