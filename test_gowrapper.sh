3f5b33e17052c6ec77dd6947a105dd4261f06076
J. Ayo Akinyele <ayo@boltlabs.io>
2020-05-08 02:53:15 -0400

    debug env in travis ci

--- test_gowrapper.sh
+++ test_gowrapper.sh
@@ -3,6 +3,6 @@
 ZKCHAN_PATH="$(pwd)/target/release"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ZKCHAN_PATH
 export CGO_LDFLAGS="-L$(pwd)/target/release"
-env | grep LD
+env 
 go get -v github.com/stretchr/testify/assert
 go test -v libzkchannels.go libzkchannels_test.go
