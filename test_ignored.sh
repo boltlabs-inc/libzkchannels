5858753a545ce069bdb39c4bfc076745a2d30e9d
J. Ayo Akinyele <ayo@boltlabs.io>
2020-06-05 01:34:45 -0400

    update test script

--- test_ignored.sh
+++ test_ignored.sh
@@ -2,7 +2,5 @@
 
 redis-cli flushdb
 cargo test --release -- --ignored --nocapture
-cargo test --package zkchannels --lib -- tests::test_customer_activated_and_unlinked_correctly --exact --nocapture &
-cargo test --package zkchannels --lib -- tests::test_merchant_activated_and_unlinked_correctly --exact --nocapture &
-wait
+cargo test --release --package zkchannels --lib -- tests::test_unlink_and_pay_is_correct --exact --nocapture
 echo "Done!"
