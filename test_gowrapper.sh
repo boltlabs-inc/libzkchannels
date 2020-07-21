5f3189770d81421140ae4bbf84eaae61037cc5fd
J. Ayo Akinyele <ayo@boltlabs.io>
2020-07-20 20:35:46 -0400

    get latest go testify module

--- test_gowrapper.sh
+++ test_gowrapper.sh
@@ -3,5 +3,5 @@
 ZKCHAN_PATH="$(pwd)/target/release"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ZKCHAN_PATH
 export CGO_LDFLAGS="-L$(pwd)/target/release"
-go get -v github.com/stretchr/testify/assert
+go get -u github.com/stretchr/testify/assert
 go test -v libzkchannels.go libzkchannels_test.go
