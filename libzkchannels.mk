3b204902adb4bc34a1695ce86c15fff67448e803
J. Ayo Akinyele <ayo@boltlabs.io>
2020-08-12 13:29:10 -0400

    more clean up

--- libzkchannels.mk
+++
@@ -1,2 +0,0 @@
-export CGO_LDFLAGS="-L/Users/dariuscognac/Documents/GitHub/lnd/libzkchannels/target/release"
-export LD_LIBRARY_PATH=/Users/dariuscognac/Documents/GitHub/lnd/libzkchannels/deps/root/lib::/Users/dariuscognac/Documents/GitHub/lnd/libzkchannels/target/release:/Users/dariuscognac/Documents/GitHub/lnd/libzkchannels/deps/root/lib
