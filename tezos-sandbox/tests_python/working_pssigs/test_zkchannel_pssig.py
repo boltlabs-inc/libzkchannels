70f9256ceb41e3293e8fed2a7ebb485f2984b84d
Darius Parvin <darius@berkeley.edu>
2020-11-16 10:54:43 +0000

    add mutualClose scenario

--- tezos-sandbox/tests_python/working_pssigs/test_zkchannel_pssig.py
+++ tezos-sandbox/tests_python/working_pssigs/test_zkchannel_pssig.py
@@ -10,13 +10,9 @@ def form_initial_storage(chan_id, cust_addr, cust_pk, merch_addr, merch_pk, cust
 
     return '(Pair (Pair (Pair {chan_id} (Pair \"{cust_addr}\" 0)) (Pair (Pair {cust_bal0} \"{cust_pk}\") (Pair "0" \"{merch_addr}\"))) (Pair (Pair 0 (Pair {merch_bal0} \"{merch_pk}\")) (Pair (Pair \"{pssig_addr}\"  {rev_lock}) (Pair {self_delay} "awaitingFunding"))))'.format(chan_id=chan_id, cust_addr=cust_addr, cust_pk=cust_pk, merch_addr=merch_addr, merch_pk=merch_pk, cust_bal0=cust_bal0, merch_bal0=merch_bal0, self_delay=self_delay, rev_lock=rev_lock, pssig_addr = pssig_addr)
 
-def form_closing_state(chan_id, cust_addr, merch_addr, cust_bal_mt, merch_bal_mt, new_rev_lock):
-
-    return '(Pair (Pair \"{chan_id}\" (Pair \"{cust_addr}\" \"{merch_addr}\")) (Pair {cust_bal_mt} (Pair {merch_bal_mt} {rev_lock})))'.format(chan_id=chan_id, cust_addr=cust_addr, merch_addr=merch_addr, cust_bal_mt=cust_bal_mt, merch_bal_mt=merch_bal_mt, rev_lock=new_rev_lock)
-
 def form_mutual_state(chan_id, cust_addr, merch_addr, cust_bal_mt, merch_bal_mt):
 
-    return '(Pair (Pair \"{chan_id}\" \"{cust_addr}\") (Pair \"{merch_addr}\" (Pair {cust_bal_mt} {merch_bal_mt} )))'.format(chan_id=chan_id, cust_addr=cust_addr, merch_addr=merch_addr, cust_bal_mt=cust_bal_mt, merch_bal_mt=merch_bal_mt)
+    return '(Pair (Pair {chan_id} \"{cust_addr}\") (Pair {cust_bal_mt} (Pair \"{merch_addr}\" {merch_bal_mt} )))'.format(chan_id=chan_id, cust_addr=cust_addr, merch_addr=merch_addr, cust_bal_mt=cust_bal_mt, merch_bal_mt=merch_bal_mt)
 
 def read_json_file(json_file):
     f = open(json_file)
@@ -85,9 +81,9 @@ def scenario_cust_close(contract_path, pubkey, message, signature):
         sandbox.client(0).originate(pssig_name, 0, "bootstrap1", pssig_contract, args)
         sandbox.client(0).bake('baker5', BAKE_ARGS)
 
-        cust_costs = dict()
+        entrypoint_cost = dict()
         current_bal = sandbox.client(0).get_balance(cust_addr)
-        cust_costs["pssig"] = cust_bal_start - current_bal
+        entrypoint_cost["pssig"] = cust_bal_start - current_bal
 
         # Define initial storage and channel variables
         contract = contract_path + "zkchannel_main.tz"
@@ -118,7 +114,7 @@ def scenario_cust_close(contract_path, pubkey, message, signature):
 
         old_bal = current_bal
         current_bal = sandbox.client(0).get_balance(cust_addr)
-        cust_costs["zkchannel"] = old_bal - current_bal
+        entrypoint_cost["zkchannel"] = old_bal - current_bal
 
         # Add customer's funds
         sandbox.client(0).transfer(cust_bal, 'bootstrap1', contract_name,
@@ -136,7 +132,7 @@ def scenario_cust_close(contract_path, pubkey, message, signature):
 
         old_bal = current_bal
         current_bal = sandbox.client(0).get_balance(cust_addr)
-        cust_costs["addFunding"] = old_bal - cust_bal - current_bal
+        entrypoint_cost["addFunding"] = old_bal - cust_bal - current_bal
 
         # Merchant initiates merch close
         sandbox.client(0).transfer(0, 'bootstrap2', contract_name,
@@ -147,7 +143,7 @@ def scenario_cust_close(contract_path, pubkey, message, signature):
 
 
         merch_current_bal = sandbox.client(0).get_balance(merch_addr)
-        merchClose_cost = merch_old_bal - merch_current_bal
+        entrypoint_cost["merchClose"]  = merch_old_bal - merch_current_bal
         
         # A final payment happens - Merchant signs off on chanID, balances,
         # revlock (and for now addresses, although that may change)
@@ -185,7 +181,7 @@ def scenario_cust_close(contract_path, pubkey, message, signature):
 
         old_bal = current_bal
         current_bal = sandbox.client(0).get_balance(cust_addr)
-        cust_costs["custClose"] = old_bal - current_bal
+        entrypoint_cost["custClose"] = old_bal - current_bal
 
         # Custer claims their balance with custClaim
         sandbox.client(0).transfer(0, 'bootstrap1', contract_name,
@@ -196,21 +192,140 @@ def scenario_cust_close(contract_path, pubkey, message, signature):
 
         old_bal = current_bal
         current_bal = sandbox.client(0).get_balance(cust_addr)
-        cust_costs["custClaim"] = old_bal - (current_bal - new_cust_bal)
+        entrypoint_cost["custClaim"] = old_bal - (current_bal - new_cust_bal)
 
         # Make sure every tez has been accounted for
         assert cust_bal_start == (
             current_bal
-            + sum(cust_costs.values())
+            + sum(entrypoint_cost.values()) - entrypoint_cost["merchClose"]
             + cust_bal
             - new_cust_bal
             )
 
         print("Cost incurred when calling the following entrypoints (tez):")
-        for k, v in cust_costs.items():
+        for k, v in entrypoint_cost.items():
             print(k + ": " + str(v))
-        print("merchClose: " + str(merchClose_cost))
 
+        return 
+
+def scenario_mutual_close(contract_path, message):
+    """ a private tezos network, initialized with network parameters
+        and some accounts. """
+    with Sandbox(paths.TEZOS_HOME, constants.IDENTITIES) as sandbox:
+        # Launch node running protocol alpha
+        sandbox.add_node(0, params=constants.NODE_PARAMS)
+        utils.activate_alpha(sandbox.client(0))
+        sandbox.add_baker(0, 'baker5', proto=constants.ALPHA_DAEMON)
+        time.sleep(5)
+        burncap = "9"
+
+        cust_addr = constants.IDENTITIES['bootstrap1']['identity']
+        cust_pk = constants.IDENTITIES['bootstrap1']['public']
+        merch_addr = constants.IDENTITIES['bootstrap2']['identity']
+        merch_pk = constants.IDENTITIES['bootstrap2']['public']
+        
+        cust_bal_start = sandbox.client(0).get_balance(cust_addr)
+
+        # Originate pssigs contract
+        pssig_contract = contract_path + "pssig.tz"
+        pssig_name = "pssig_contract"
+        args = ["--init", "Unit", "--burn-cap", burncap]
+
+        sandbox.client(0).originate(pssig_name, 0, "bootstrap1", pssig_contract, args)
+        sandbox.client(0).bake('baker5', BAKE_ARGS)
+
+        entrypoint_cost = dict()
+        current_bal = sandbox.client(0).get_balance(cust_addr)
+        entrypoint_cost["pssig"] = cust_bal_start - current_bal
+
+        # Define initial storage and channel variables
+        contract = contract_path + "zkchannel_main.tz"
+        chan_id_fr, rev_lock_fr, cust_bal_fr, merch_bal_fr = message
+        sig_s1, sig_s2 = signature
+
+        contract_name = "my_zkchannel"
+        chan_id = chan_id_fr
+        cust_bal = 20
+        merch_bal = 10
+        cust_bal_mt = cust_bal * 1000000
+        merch_bal_mt = merch_bal * 1000000
+        # Balance in mutez as bytes
+        cust_bal_b = cust_bal_fr 
+        merch_bal_b = merch_bal_fr 
+        rev_lock0 = "0x1f98c84caf714d00ede5d23142bc166d84f8cd42adc18be22c3d47453853ea49"
+        # self_delay = 86400    # seconds in 1 day (60*60*24)
+        self_delay = 3
+
+        pssig_addr = sandbox.client(0).get_contract_address(pssig_name)
+
+        # Originate zkchannel contract (without funding)
+        initial_storage = form_initial_storage(chan_id, cust_addr, cust_pk, merch_addr, merch_pk, cust_bal_mt, merch_bal_mt, rev_lock0, self_delay, pssig_addr)
+        args = ["--init", initial_storage, "--burn-cap", burncap]
+        sandbox.client(0).originate(contract_name, 0, "bootstrap1", contract, args)
+        
+        sandbox.client(0).bake('baker5', BAKE_ARGS)
+
+        old_bal = current_bal
+        current_bal = sandbox.client(0).get_balance(cust_addr)
+        entrypoint_cost["zkchannel"] = old_bal - current_bal
+
+        # Add customer's funds
+        sandbox.client(0).transfer(cust_bal, 'bootstrap1', contract_name,
+                                   ['--entrypoint', 'addFunding',
+                                    '--burn-cap', burncap])
+
+        # Add merchant's funds
+        sandbox.client(0).transfer(merch_bal, 'bootstrap2', contract_name,
+                                   ['--entrypoint', 'addFunding',
+                                    '--burn-cap', burncap])
+
+        sandbox.client(0).bake('baker5', BAKE_ARGS)
+
+        merch_old_bal = sandbox.client(0).get_balance(merch_addr)
+
+        old_bal = current_bal
+        current_bal = sandbox.client(0).get_balance(cust_addr)
+        entrypoint_cost["addFunding"] = old_bal - cust_bal - current_bal
+
+        # Create the mutual close state that customer and merchant settle on
+        new_cust_bal = 1
+        new_merch_bal = 29
+        new_cust_bal_mt = new_cust_bal * 1000000
+        new_merch_bal_mt = new_merch_bal * 1000000
+        mutual_state = form_mutual_state(chan_id, cust_addr, merch_addr, new_cust_bal_mt, new_merch_bal_mt)
+
+        # Cust and Merch signs off on mutual close state
+        mutual_type = 'pair (pair bls12_381_fr address) (pair mutez (pair address mutez))'
+        packed = sandbox.client(0).pack(mutual_state, mutual_type)
+        cust_sig = sandbox.client(0).sign_bytes_of_string(packed, "bootstrap1")
+        merch_sig = sandbox.client(0).sign_bytes_of_string(packed, "bootstrap2")
+
+        storage = '(Pair (Pair {cust_bal_mt} \"{cust_sig}\") (Pair {merch_bal_mt} \"{merch_sig}\"))'.format(cust_sig=cust_sig, merch_sig=merch_sig, cust_bal_mt=new_cust_bal_mt, merch_bal_mt=new_merch_bal_mt)
+
+        sandbox.client(0).transfer(0, 'bootstrap1', contract_name,
+                                   ['--entrypoint', 'mutualClose',
+                                    '--burn-cap', burncap,
+                                    '--arg', storage])
+
+        sandbox.client(0).bake('baker5', BAKE_ARGS)
+
+        old_bal = current_bal
+        current_bal = sandbox.client(0).get_balance(cust_addr)
+        entrypoint_cost["mutualClose"] = old_bal + new_cust_bal - current_bal
+
+        # Make sure every tez has been accounted for
+        assert cust_bal_start == (
+            current_bal
+            + sum(entrypoint_cost.values())
+            + cust_bal
+            - new_cust_bal
+            )
+        
+        print("Cost incurred when calling the following entrypoints (tez):")
+        for k, v in entrypoint_cost.items():
+            print(k + ": " + str(v))
+
+        return 
 
 if __name__ == "__main__":
     contract_path = sys.argv[1]
@@ -226,3 +341,6 @@ if __name__ == "__main__":
     print("signature: ", signature)
 
     scenario_cust_close(contract_path, merch_pk, message, signature)
+    scenario_mutual_close(contract_path, message)
+
+
