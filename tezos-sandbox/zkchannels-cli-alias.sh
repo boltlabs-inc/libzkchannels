af8840f2a2350818c484af351c4af99bcf7d8fa4
Darius Parvin <darius@berkeley.edu>
2021-03-01 16:34:47 -0800

    Update zkchannels-cli-alias.sh

--- tezos-sandbox/zkchannels-cli-alias.sh
+++ tezos-sandbox/zkchannels-cli-alias.sh
@@ -1,9 +1,9 @@
 alias zkchannels-cust-open="zkchannels-cli open --party CUST --other-port 12347 --own-port 12346"
-alias zkchannels-cust-init="zkchannels-cli init --party CUST --other-port 12347 --own-port 12346 --input-amount 0 --output-amount 30000000"
+alias zkchannels-cust-init="zkchannels-cli init --party CUST --other-port 12347 --own-port 12346"
 alias zkchannels-cust-activate="zkchannels-cli activate --party CUST --other-port 12347 --own-port 12346"
 alias zkchannels-cust-unlink="zkchannels-cli unlink --party CUST --other-port 12347 --own-port 12346"
 alias zkchannels-cust-pay="zkchannels-cli pay --party CUST --other-port 12347 --own-port 12346"
-alias zkchannels-cust-close="zkchannels-cli close --party CUST --other-port 12347 --own-port 12346 --decompress-cust-close --file cust_close.json"
+alias zkchannels-cust-close="zkchannels-cli close --party CUST --other-port 12347 --own-port 12346 --decompress-cust-close"
 
 alias zkchannels-merch-open="zkchannels-cli open --party MERCH --own-port 12347 --other-port 12346"
 alias zkchannels-merch-init="zkchannels-cli init --party MERCH --own-port 12347 --other-port 12346"
