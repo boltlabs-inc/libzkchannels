3c5edc362b5ec08fc3f4b005b0f28874348b2aa6
Darius Parvin <darius@berkeley.edu>
2021-03-12 13:28:57 -0800

    complete zkchannel_edo2net_broadcaster.py

--- tezos-sandbox/watchtower/edo2net_test/zkchannel_edo2net_broadcaster.py
+++ tezos-sandbox/watchtower/edo2net_test/zkchannel_edo2net_broadcaster.py
@@ -1,8 +1,11 @@
+# Example usage:
+# python3 zkchannel_edo2net_broadcaster.py --cust=tz1S6eSPZVQzHyPF2bRKhSKZhDZZSikB3e51.json --merch=tz1VcYZwxQoyxfjhpNiRkdCUe5rzs53LMev6.json --close=sample_cust_close.json 
+
+import argparse
 from pytezos import pytezos
 from pytezos import Contract
 from pytezos import ContractInterface
 import json
-import pprint
 
 def read_json_file(json_file):
     f = open(json_file)
@@ -42,135 +45,169 @@ def get_cust_close_token(data):
     # print("Signature: %s" % signature)
     return (pubkey, message, signature)
 
+def convert_mt_to_tez(balance):
+    return str(int(balance) /1000000)
 
-# pytezos = pytezos.using(shell='http://ec2-54-234-50-176.compute-1.amazonaws.com:18732')
+if __name__ == "__main__":
+        
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--shell", "-n", help="the address to connect to edo2net", default = "https://rpc.tzkt.io/edo2net/")
+    parser.add_argument("--cust", "-c", help="customer's testnet json file")
+    parser.add_argument("--merch", "-m", help="merchant's testnet json file")
+    parser.add_argument("--close", "-cc", help="Enter the filename (with path) to the cust_close.json file created by zkchannels-cli")
+    args = parser.parse_args()
 
-cust_key = "tz1Q8qQ2FYRSJ8dKj8ukRMRXRcv41dssg1pN.json"
-merch_key = "tz1axtoa27aKYaAj1TZVLXuBPMV6Z1XrhLoX.json"
+    if args.shell:
+        pytezos = pytezos.using(shell=args.shell)
+    print("Connecting to edo2net via: " + args.shell)
+    cust_json = args.cust
+    merch_json = args.merch
 
-# Set customer pytezos interface
-cust_py = pytezos.using(key=cust_key)
-cust_close_json = read_json_file(cust_key)
-cust_addr = cust_close_json['pkh']
-cust_py.activate_account().fill().sign().inject(_async=False)
-cust_out = cust_py.reveal().autofill().sign().inject()
-cust_pubkey = cust_out['contents'][0]['public_key']
+    # Set customer and merch pytezos interfaces
+    cust_py = pytezos.using(key=cust_json)
+    cust_addr = read_json_file(cust_json)['pkh']
+    merch_py = pytezos.using(key=merch_json)
+    merch_addr = read_json_file(merch_json)['pkh']
 
-# Set merchant pytezos interface
-merch_py = pytezos.using(key=merch_key)
-merch_close_json = read_json_file(merch_key)
-merch_addr = merch_close_json['pkh']
-merch_py.activate_account().fill().sign().inject(_async=False)
-merch_out = merch_py.reveal().autofill().sign().inject()
-merch_pubkey = merch_out['contents'][0]['public_key']
+    # load cust_close json from libzkchannels
+    cust_close_json = read_json_file('sample_cust_close.json')
+    # load zchannel contracts
+    pssig_code = ContractInterface.from_file('zkchannel_pssig.tz')
+    main_code = ContractInterface.from_file('zkchannel_main.tz')
 
-pssig_code = ContractInterface.from_file('zkchannel_pssig.tz')
-pssig_ci = pssig_code.using(key=cust_key)
+    # Activate cust and merch testnet accounts
+    try:
+        print("Activating cust account")
+        cust_py.activate_account().fill().sign().inject()
+    except:
+        print("Cust account already activated")
 
-# Originate the pssig contract
-out = cust_py.origination(script=pssig_ci.script()).autofill().sign().inject(_async=False)
+    try:
+        print("Revealing cust pubkey")
+        out = cust_py.reveal().autofill().sign().inject()
+    except:
+        pass
+    cust_pubkey = cust_py.key.public_key()
 
-# Get the address of the pssig contract so we can reference it in the 
-# main zkchannel contract
-opg = pytezos.shell.blocks[-20:].find_operation(out['hash'])
-pssig_id = opg['contents'][0]['metadata']['operation_result']['originated_contracts'][0]
+    try:
+        print("Activating merch account")
+        merch_py.activate_account().fill().sign().inject()
+    except: 
+        print("Merch account already activated")
 
-main_code = ContractInterface.from_file('zkchannel_main.tz')
+    try:
+        print("Revealing merch pubkey")
+        out = merch_py.reveal().autofill().sign().inject()
+    except:
+        pass
+    merch_pubkey = merch_py.key.public_key()
 
-cust_close_json = read_json_file('sample_cust_close.json')
-(pubkey, message, signature) = get_cust_close_token(cust_close_json)
-chan_id_fr, rev_lock_fr, cust_bal_fr, merch_bal_fr = message
-sig_s1, sig_s2 = signature
+    # Originate the pssig contract
+    pssig_ci = pssig_code.using(key=cust_json)
+    print("Originate pssig contract")
+    out = cust_py.origination(script=pssig_ci.script()).autofill().sign().inject(_async=False)
+    print("Originate pssig contract ophash: ", out['hash'])
+    # Get the address of the pssig contract so we can reference it in the 
+    # main zkchannel contract
+    opg = pytezos.shell.blocks[-20:].find_operation(out['hash'])
+    pssig_id = opg['contents'][0]['metadata']['operation_result']['originated_contracts'][0]
+    print("pssig contract address: ", pssig_id)
 
-main_storage = {'chanID': chan_id_fr, 
-'custAddr': cust_addr, 
-'custBal':0, 
-'custFunding': 20000000, 
-'custPk': cust_pubkey, 
-'delayExpiry': '1970-01-01T00:00:00Z', 
-'merchAddr': merch_addr, 
-'merchBal': 0, 
-'merchFunding': 10000000, 
-'merchPk': merch_pubkey, 
-'pssigContract': pssig_id, 
-'revLock': '1f98c84caf714d00ede5d23142bc166d84f8cd42adc18be22c3d47453853ea49', 
-'selfDelay': 3, 
-'status': 0}
+    # Create initial storage for main zkchannel contract
+    (pubkey, message, signature) = get_cust_close_token(cust_close_json)
+    chan_id_fr, rev_lock_fr, cust_bal_fr, merch_bal_fr = message
 
-out = cust_py.origination(script=main_code.script(initial_storage=main_storage)).autofill().sign().inject(_async=False)
+    main_storage = {'chanID': chan_id_fr, 
+    'custAddr': cust_addr, 
+    'custBal':0, 
+    'custFunding': 20000000, 
+    'custPk': cust_pubkey, 
+    'delayExpiry': '1970-01-01T00:00:00Z', 
+    'merchAddr': merch_addr, 
+    'merchBal': 0, 
+    'merchFunding': 10000000, 
+    'merchPk': merch_pubkey, 
+    'pssigContract': pssig_id, 
+    'revLock': '0x1f98c84caf714d00ede5d23142bc166d84f8cd42adc18be22c3d47453853ea49', 
+    'selfDelay': 3, 
+    'status': 0}
 
-opg = pytezos.shell.blocks[-20:].find_operation(out['hash'])
-main_id = opg['contents'][0]['metadata']['operation_result']['originated_contracts'][0]
+    # Originate main zkchannel contract
+    print("Originate main zkChannel contract")
+    out = cust_py.origination(script=main_code.script(initial_storage=main_storage)).autofill().sign().inject(_async=False)
+    print("Originate zkChannel ophash: ", out['hash'])
+    # Get address of main zkchannel contract
+    opg = pytezos.shell.blocks[-20:].find_operation(out['hash'])
+    main_id = opg['contents'][0]['metadata']['operation_result']['originated_contracts'][0]
+    print("zkChannel contract address: ", main_id)
 
-cust_ci = cust_py.contract(main_id)
+    # Set contract interfaces for cust and merch
+    cust_ci = cust_py.contract(main_id)
+    merch_ci = merch_py.contract(main_id)
 
-merch_ci = merch_py.contract(main_id)
+    print("Adding customer funding")
+    out = cust_ci.addFunding().with_amount(20000000).inject(_async=False)
+    print("Cust Add Funding ophash: ", out['hash'])
 
-# watchtower_command = "python3 passive_zkchannel_watchtower.py --contract {cid} --network http://localhost:18731 --identity merchant".format(cid=main_id)
+    print("Adding merchant funding")
+    out = merch_ci.addFunding().with_amount(10000000).inject(_async=False)
+    print("Merch Add Funding ophash: ", out['hash'])
 
-# print("Run the watchtower with \n" + watchtower_command)
+    print("Broadcasting Merch Close")
+    out = merch_ci.merchClose().inject(_async=False)
+    print("Merch Close ophash: ", out['hash'])
 
-out = cust_ci.addFunding().with_amount(20000000).inject(_async=False)
+    # Form cust close storage
+    (pubkey, message, signature) = get_cust_close_token(cust_close_json)
+    chan_id_fr, rev_lock_fr, cust_bal_fr, merch_bal_fr = message
+    sig_s1, sig_s2 = signature
+    new_cust_bal_mt = cust_close_json["message"]["cust_bal"]
+    new_merch_bal_mt = cust_close_json["message"]["merch_bal"]
+    new_cust_bal = convert_mt_to_tez(new_cust_bal_mt)
+    new_merch_bal = convert_mt_to_tez(new_merch_bal_mt)
 
-# input("Bake to add cust funding")
+    s1 = sig_s1 
+    s2 = sig_s2 
+    g2 = pubkey.get("g2") 
+    merchPk0 = pubkey.get("Y0") 
+    merchPk1 = pubkey.get("Y1") 
+    merchPk2 = pubkey.get("Y2") 
+    merchPk3 = pubkey.get("Y3") 
+    merchPk4 = pubkey.get("X") 
 
-out = merch_ci.addFunding().with_amount(10000000).inject(_async=False)
+    # storage = '\'(Pair (Pair (Pair {custBal} {g2}) (Pair {merchBal} (Pair {merchPk0} {merchPk1}))) (Pair (Pair {merchPk2} (Pair {merchPk3} {merchPk4})) (Pair {rev_lock_fr} (Pair {s1} {s2}))))\''.format(s1=s1, s2=s2, g2=g2, merchPk0=merchPk0, merchPk1=merchPk1, merchPk2=merchPk2, merchPk3=merchPk3, merchPk4=merchPk4, rev_lock_fr=rev_lock_fr, custBal=new_cust_bal_mt, merchBal=new_merch_bal_mt)
 
-# input("Bake to add merch funding")
+    close_storage = {
+        "custBal": new_cust_bal,
+        "g2": g2,
+        "merchBal": new_merch_bal,
+        "merchPk0": merchPk0,
+        "merchPk1": merchPk1,
+        "merchPk2": merchPk2,
+        "merchPk3": merchPk3,
+        "merchPk4": merchPk4,
+        "revLock": rev_lock_fr,
+        "s1": s1,
+        "s2": s2
+    }
 
-# # print(merch_ci.merchClose("").cmdline())
-out = merch_ci.merchClose().inject()
+    print("Broadcasting Cust Close")
+    # print(cust_ci.custClose(close_storage).cmdline())
+    out = cust_ci.custClose(close_storage).inject(_async=False)
+    print("Cust Close ophash: ", out['hash'])
 
-# input("Bake to confirm merchClose")
+    import pdb; pdb.set_trace();
 
+    print("Broadcasting Cust Claim")
+    out = cust_ci.custClaim().inject()
+    print("Cust Claim ophash: ", out['hash'])
 
+    # # Alternatively, to close the channel with merchDispute, run:
+    # print("Dry run of Cust Claim")
+    # out = cust_ci.custClaim().run_operation()
+    # print("Cust Claim valid: ", out.operations[0]['internal'])
+    # print("Broadcasting Merch Dispute")
+    # rev_secret = "enter secret here"
+    # merch_ci.merchDispute(rev_secret).inject()
 
-cust_close_json = read_json_file('sample_cust_close.json')
-(pubkey, message, signature) = get_cust_close_token(cust_close_json)
-chan_id_fr, rev_lock_fr, cust_bal_fr, merch_bal_fr = message
-sig_s1, sig_s2 = signature
-
-new_merch_bal_mt = cust_close_json["message"]["merch_bal"]
-new_cust_bal_mt = cust_close_json["message"]["cust_bal"]
-
-# sample signature, merch-pk and g2 
-s1 = sig_s1 
-s2 = sig_s2 
-g2 = pubkey.get("g2") 
-merchPk0 = pubkey.get("Y0") 
-merchPk1 = pubkey.get("Y1") 
-merchPk2 = pubkey.get("Y2") 
-merchPk3 = pubkey.get("Y3") 
-merchPk4 = pubkey.get("X") 
-
-storage = '\'(Pair (Pair (Pair {custBal} {g2}) (Pair {merchBal} (Pair {merchPk0} {merchPk1}))) (Pair (Pair {merchPk2} (Pair {merchPk3} {merchPk4})) (Pair {rev_lock_fr} (Pair {s1} {s2}))))\''.format(s1=s1, s2=s2, g2=g2, merchPk0=merchPk0, merchPk1=merchPk1, merchPk2=merchPk2, merchPk3=merchPk3, merchPk4=merchPk4, rev_lock_fr=rev_lock_fr, custBal=new_cust_bal_mt, merchBal=new_merch_bal_mt)
-
-
-close_storage = {
-    "custBal": new_cust_bal_mt,
-    "g2": g2,
-    "merchBal": new_merch_bal_mt,
-    "merchPk0": merchPk0,
-    "merchPk1": merchPk1,
-    "merchPk2": merchPk2,
-    "merchPk3": merchPk3,
-    "merchPk4": merchPk4,
-    "revLock": rev_lock_fr,
-    "s1": s1,
-    "s2": s2
-}
-
-import pdb; pdb.set_trace();
-# # print(cust_ci.custClose(close_storage).cmdline())
-out = cust_ci.custClose(close_storage).inject(_async=False)
-
-# input("Bake to confirm custClose")
-
-# cust_ci.custClaim("").inject()
-# print("Bake to confirm custClaim and close the channel")
-
-# # # Alternatively, to close the channel with merchDispute, run:
-# # rev_secret = "123456789ccc"
-# # merch_ci.dis(close_storage).inject()
-# # print("Bake to confirm merchDispute and close the channel")
-
+    print("Tests finished!")
